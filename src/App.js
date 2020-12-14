bbb15d71640fd5d5406bc2dd7ec79d5af73bfdf6
Elena_Bokhan <clevergirl_777@mail.ru>
2020-12-15 00:36:50 +0400

    add start and pause methods

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import backUrl from './background.jpg';
 import './App.css';
 
 class App extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props)
 		this.state = {
 			width: 10,
 			height: 10,
-			countTable:100,			
-			livingCells:[],	
-			timerOutId: undefined,
+			countTable: 100,
+			livingCells: [],
+			timerId: undefined,
 			// init()
 		}
 	}
@@ -21,59 +21,77 @@ class App extends Component {
 	// 	console.log("Введите размер высоты поля");
 	// 	let heightT = console.ReadLine();
 	// }
-	componentDidMount(){
+	componentDidMount() {
 		const arr = new Set();
-		while(arr.size<30){
-			let number = Math.floor(Math.random()*100 + 1);
+		while (arr.size < 30) {
+			let number = Math.floor(Math.random() * 100 + 1);
 			arr.add(number);
 		}
-		this.setState({livingCells:Array.from(arr)});
-		this.setState({timerOutId: setInterval(() =>{
-			this.setState({livingCells: this.liveOrDie()})
-			console.log("work")}, 1000)})
+		this.setState({ livingCells: Array.from(arr) });		
 	}
-	liveOrDie(){
+	start = () => {
+		this.setState({
+			timerId: setInterval(() => {
+				this.setState({ livingCells: this.liveOrDie() });
+				this.exportToConsole();
+			}, 1000)
+		})
+	}
+	pause = () => {
+		clearTimeout(this.state.timerId);
+	}
+	liveOrDie() {
 		const currentAlive = [];
 		[...Array(100).keys()].forEach(el => {
 			const neibors = this.getNeighbours(el);
-			const aliveNeibors = neibors.filter(item=>this.state.livingCells.includes(item));
-			if(this.state.livingCells.includes(el) && (aliveNeibors.length===2 || aliveNeibors.length===3)){
+			const aliveNeibors = neibors.filter(item => this.state.livingCells.includes(item));
+			if 	(this.state.livingCells.includes(el) && (aliveNeibors.length === 2 || aliveNeibors.length === 3) || (!this.state.livingCells.includes(el) && aliveNeibors.length === 3)) {
 				currentAlive.push(el)
-			} else {
-				if (aliveNeibors.length === 3) currentAlive.push(el)
-			}
+			} 
 		})
-		return currentAlive
+		return currentAlive;
 	}
-
-	componentWillUnmount(){		
-		clearTimeout(this.state.timerOutId);
-	}
-	
 	getNeighbours = cell => {
 		let neightbours;
-		if (cell%10 === 9 || cell === 9) neightbours = [cell-1,cell-11,cell-10,cell+9,cell+10]; 
-		else if (cell%10 === 0) neightbours = [cell-10,cell-9,cell+1,cell+10,cell+11]; 		
-		else neightbours = [cell+1,cell-1,cell-11,cell-10,cell-9,cell+9,cell+10,cell+11];  
-		return neightbours  
+		if (cell % 10 === 9 || cell === 9) {
+			return neightbours = [cell - 1, cell - 11, cell - 10, cell + 9, cell + 10]}
+		else if (cell % 10 === 0) {
+			return neightbours = [cell - 10, cell - 9, cell + 1, cell + 10, cell + 11]}
+		else neightbours = [cell + 1, cell - 1, cell - 11, cell - 10, cell - 9, cell + 9, cell + 10, cell + 11];
+		return neightbours;
 	}
-	render(){		
-		return (<div style = {{backgroundImage: `url(${ backUrl })`}} className = "back">
-					<h1>Game of Life</h1>
-					<h3>test task</h3>
-					<div className = "field-wrap">
-						<div className="field" 	style = {{width: `${this.state.width*40}px`, height: `${this.state.height*40}px`}}>
-							{[...Array(this.state.width*this.state.height).keys()].map( item => {
-								return	<div  	className = {  this.state.livingCells.includes(item) ?  "alive" : "dead"}
-												data-num = { item } 												 
-												key = { item }>
-										</div>
-							})} 
-						</div>
-					</div>
+	exportToConsole = () => {
+		let str = "";
+		for(let i=0; i<this.state.width*this.state.height;i++){
+			if (i===0 || i%10 === 0) str+="|";
+			this.state.livingCells.includes(i) ? str+="x|" : str+="-|";
+			if (i===9 || i%10 === 9) str+="\n";
+		}
+		console.log(str);
+		console.log("=".repeat(this.state.width*2+1));
+	}
+	render() {
+		return (<div style={{ backgroundImage: `url(${backUrl})` }} className="back">
+			<div className="field-wrap">
+				<h1>Game of Life</h1>
+				<h3>test task</h3>
+				<div className="buttons-wrap">
+					<button onClick = {() => this.start()}>Start</button>
+					<button onClick = {() => this.pause()}>Pause</button>
 				</div>
+				
+				<div className="field" style={{ width: `${this.state.width * 40}px`, height: `${this.state.height * 40}px` }}>
+					{[...Array(this.state.width * this.state.height).keys()].map(item => {
+						return <div className={this.state.livingCells.includes(item) ? "alive" : "dead"}
+							data-num={item}
+							key={item}>
+						</div>
+					})}
+				</div>
+			</div>
+		</div>
 		);
-	}  
+	}
 }
 
 export default App;
