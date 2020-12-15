1b0bcb2addeaa6c26bcb00b8504215ce2857b3ec
Elena_Bokhan <clevergirl_777@mail.ru>
2020-12-15 17:59:29 +0400

    setWidth and setHeight methods was added

--- src/App.js
+++ src/App.js
@@ -7,20 +7,13 @@ class App extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			width: 10,
-			height: 10,
-			countTable: 100,
+			width: "",
+			height: "",
 			livingCells: [],
 			timerId: undefined,
-			// init()
+			initial: false
 		}
 	}
-	// componentWillMount(){
-	// 	console.log("Введите размер ширины поля")
-	// 	let widthT = console.ReadLine();
-	// 	console.log("Введите размер высоты поля");
-	// 	let heightT = console.ReadLine();
-	// }
 	componentDidMount() {
 		const arr = new Set();
 		while (arr.size < 30) {
@@ -45,52 +38,88 @@ class App extends Component {
 		[...Array(100).keys()].forEach(el => {
 			const neibors = this.getNeighbours(el);
 			const aliveNeibors = neibors.filter(item => this.state.livingCells.includes(item));
-			if 	(this.state.livingCells.includes(el) && (aliveNeibors.length === 2 || aliveNeibors.length === 3) || (!this.state.livingCells.includes(el) && aliveNeibors.length === 3)) {
+
+			if ((this.state.livingCells.includes(el) 
+			&& (aliveNeibors.length === 2 || aliveNeibors.length === 3)) 
+			|| (!this.state.livingCells.includes(el) 
+			&& aliveNeibors.length === 3)) {
 				currentAlive.push(el)
 			} 
 		})
 		return currentAlive;
 	}
-	getNeighbours = cell => {
-		let neightbours;
-		if (cell % 10 === 9 || cell === 9) {
-			return neightbours = [cell - 1, cell - 11, cell - 10, cell + 9, cell + 10]}
-		else if (cell % 10 === 0) {
-			return neightbours = [cell - 10, cell - 9, cell + 1, cell + 10, cell + 11]}
-		else neightbours = [cell + 1, cell - 1, cell - 11, cell - 10, cell - 9, cell + 9, cell + 10, cell + 11];
-		return neightbours;
+	getNeighbours = cell => {		
+		const sizeW = +this.state.width;
+		const sizeH = +this.state.height;
+		const possibleNeibors = [cell + 1, cell - 1, cell - sizeW - 1, cell - sizeW,
+								 cell - sizeW + 1, cell + sizeW - 1, 
+								 cell + sizeW, cell + sizeW + 1];
+		return possibleNeibors.filter(el => el>=0 && el<sizeW*sizeH)
+		// try{
+
+		// } catch(e){
+
+		// } 
+		// if (cell % 10 === 9 || cell === 9) {
+		// 	return neightbours = [cell - 1, cell - 11, cell - 10, cell + 9, cell + 10]}
+		// else if (cell % 10 === 0) {
+		// 	return neightbours = [cell - 10, cell - 9, cell + 1, cell + 10, cell + 11]}
+		// else neightbours = [cell + 1, cell - 1, cell - 11, cell - 10, cell - 9, cell + 9, cell + 10, cell + 11];
+		// return neightbours;
 	}
 	exportToConsole = () => {
 		let str = "";
 		for(let i=0; i<this.state.width*this.state.height;i++){
-			if (i===0 || i%10 === 0) str+="|";
+			if (i===0 || i%this.state.width === 0) str+="|";
 			this.state.livingCells.includes(i) ? str+="x|" : str+="-|";
-			if (i===9 || i%10 === 9) str+="\n";
+			if (i+1===this.state.width || (i+1)%this.state.width === 0) str+="\n";
 		}
 		console.log(str);
 		console.log("=".repeat(this.state.width*2+1));
 	}
+	initSize = () => {
+		this.setState({ initial: true });	
+	}
+	setHeight = (e) => {
+		this.setState({ height: e.target.value });	
+	}
+	setWidth = (e) => {
+		this.setState({ width: e.target.value });	
+	}
 	render() {
-		return (<div style={{ backgroundImage: `url(${backUrl})` }} className="back">
-			<div className="field-wrap">
-				<h1>Game of Life</h1>
-				<h3>test task</h3>
-				<div className="buttons-wrap">
-					<button onClick = {() => this.start()}>Start</button>
-					<button onClick = {() => this.pause()}>Pause</button>
-				</div>
-				
-				<div className="field" style={{ width: `${this.state.width * 40}px`, height: `${this.state.height * 40}px` }}>
-					{[...Array(this.state.width * this.state.height).keys()].map(item => {
-						return <div className={this.state.livingCells.includes(item) ? "alive" : "dead"}
-							data-num={item}
-							key={item}>
-						</div>
-					})}
-				</div>
-			</div>
-		</div>
-		);
+		return  <div style={{ backgroundImage: `url(${backUrl})` }} className="back">
+					<div className="field-wrap">
+						<h1>Game of Life</h1>
+						<h3>test task</h3>
+						{!this.state.initial 
+						? <div className="init-size-field">
+								<span>Введите размер ширины поля</span>
+								<input onChange = {(e) => this.setWidth(e)} 
+									   value = {this.state.width}/>
+								<span>Введите размер высоты поля</span>
+								<input onChange = {(e) => this.setHeight(e)} 
+								 	   value = {this.state.height}/>
+								<button onClick = {() => this.initSize()}>OK</button>
+							</div>
+						: <>
+							<div className="buttons-wrap">
+								<button onClick = {() => this.start()}>Start</button>
+								<button onClick = {() => this.pause()}>Pause</button>
+							</div>
+							<div className="field" 
+								 style={{ width: `${this.state.width * 40}px`, 
+										height: `${this.state.height * 40}px` }}>
+								{[...Array(this.state.width * this.state.height).keys()].map(item => {
+									return <div className={this.state.livingCells.includes(item) ? "alive" : "dead"}
+												data-num={item}
+												key={item}>
+										   </div>
+								})}
+							</div>
+						  </>
+						}
+					</div>					
+				</div>		
 	}
 }
 
