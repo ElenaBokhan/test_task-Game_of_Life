3a27b0a64a9fc8cae2fc6d68f6ba12ecbd20c099
Elena_Bokhan <clevergirl_777@mail.ru>
2020-12-16 15:34:44 +0400

    add compare state method

--- src/App.js
+++ src/App.js
@@ -1,260 +1,156 @@
 import './App.css';
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import backUrl from './background.jpg';
 import './App.css';
 
-
-const App = () => {
-
-	const [width, setWidth] = useState("");
-	const [height, setHeight] = useState("");
-	const [percentLiveCells, setPercent] = useState("");
-	const [initial, setInitial] = useState(false);
-	const [livingCells, setLivingCells] = useState();
-	// const [timerId, setTimerId] = useState(undefined);
-	// const livingCells = [];
-	let timerId = null;
-
-	const init = () => {
-		const arr = new Set();
-		const amountLivingCell = width*height*percentLiveCells/100;
-		while (arr.size < amountLivingCell) {
-			let number = Math.floor(Math.random() * width*height + 1);
-			arr.add(number);
+class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			width: "",
+			height: "",
+			percentLivingCell: "",			
+			timerId: undefined,
+			initial: false,
+			field: [],
+			gameOver: false,
+			strState:""
 		}
-		setLivingCells(Array.from(arr));
-		setInitial(true);	
 	}
-	const addFunc = (arr) => {
-		setLivingCells(arr);
-	}
-	const start = () => {
-		 timerId = setInterval(() => {
-			 let sss = liveOrDie()
-			addFunc(sss)			
-			exportToConsole();
-		}, 1000)
-	}
-	const pause = () => {
-		clearTimeout(timerId);
-	}
-	const liveOrDie = () => {
-		const currentAlive = [];
-
-		// for(let i=0; i<this.state.width; i++){
-		// 	for(let j=0; j<this.state.height; j++){
-			
-		// 	}
-		// }
-
-		[...Array(width*height).keys()].forEach(el => {
-			const neibors = getNeighbours(el);
-			const aliveNeibors = neibors.filter(item =>livingCells.includes(item));
-
-			if ((livingCells.includes(el) 
-			&& (aliveNeibors.length === 2 || aliveNeibors.length === 3)) 
-			|| (!livingCells.includes(el) 
-			&& aliveNeibors.length === 3)) {
-				currentAlive.push(el)
-			} 
+	
+	start = () => {
+		this.setState({
+			timerId: setInterval(() => {				
+				this.setState({ field: this.getNewField() });
+				const str = this.exportToConsole();
+				this.compareState(str);
+			}, 1000)
 		})
-		return currentAlive;
 	}
-	const getNeighbours = cell => {		
-		const sizeW = +width;
-		const sizeH = +height;
-		const possibleNeibors = [cell + 1, cell - 1, cell - sizeW - 1, cell - sizeW,
-									cell - sizeW + 1, cell + sizeW - 1, 
-									cell + sizeW, cell + sizeW + 1];
-		return possibleNeibors.filter(el => el>=0 && el<sizeW*sizeH)
-		
-		// if (cell % 10 === 9 || cell === 9) {
-		// 	return neightbours = [cell - 1, cell - 11, cell - 10, cell + 9, cell + 10]}
-		// else if (cell % 10 === 0) {
-		// 	return neightbours = [cell - 10, cell - 9, cell + 1, cell + 10, cell + 11]}
-		// else neightbours = [cell + 1, cell - 1, cell - 11, cell - 10, cell - 9, cell + 9, cell + 10, cell + 11];
-		// return neightbours;
+	pause = () => {
+		clearTimeout(this.state.timerId);
 	}
-	const exportToConsole = () => {
+	getNewField() {
+		const currentField = [];
+		for(let i=0; i<this.state.height; i++){	
+			currentField[i]=[];		
+			for(let j=0; j<this.state.width; j++){
+				const neighbors = this.getLiveNeighbours(j,i);
+
+				if ((this.state.field[i][j] === 1 && (neighbors.length === 2 || neighbors.length === 3)) || 
+					(this.state.field[i][j] === 0 && neighbors.length === 3)) {
+						currentField[i].push(1);
+				} else  currentField[i].push(0);
+			}
+		}		
+		return 	currentField;
+	}
+	getLiveNeighbours = (x, y) => {	
+		const possibleNeighbors = [[x-1,y],[x+1,y],
+								 [x,y-1],[x,y+1],
+								 [x-1,y-1],[x+1,y-1],
+								 [x-1,y+1],[x+1,y+1]];
+	 	const liveNeighbors = possibleNeighbors.filter( ([col, row]) => this.state.field?.[row]?.[col] === 1);
+	 	return liveNeighbors;		
+	}
+	exportToConsole = () => {
 		let str = "";
-		for(let i=0; i<width*height;i++){
-			if (i===0 || i%width === 0) str+="|";
-			livingCells.includes(i) ? str+="x|" : str+="-|";
-			if (i+1===width || (i+1)%width === 0) str+="\n";
+		for(let i=0; i<this.state.height;i++){
+			for(let j=0; j<this.state.width;j++){
+				if (j===0) str+="|";
+				this.state.field[i][j] === 1 ? str+="x|" : str+="-|";
+				if (j===this.state.width-1) str+="\n";
+			}
 		}
 		console.log(str);
-		console.log("=".repeat(width*2+1));
-	}
-	return  <div style={{ backgroundImage: `url(${backUrl})` }} className="back">
-				<div className="field-wrap">
-					<h1>Game of Life</h1>
-					<h3>test task</h3>
-					{!initial ? <div className="init-size-field">
-									<span>Введите размер ширины поля</span>
-									<input onChange = {(e) => setWidth(e.target.value)} 
-										   value = {width}/>
-									<span>Введите размер высоты поля</span>
-									<input onChange = {(e) => setHeight(e.target.value)} 
-										   value = {height}/>
-									<span>Введите колличество живых клеток(%)</span>
-									<input onChange = {(e) => setPercent(e.target.value)} 
-										   value = {percentLiveCells}/>
-									<button onClick = {init}>OK</button>
-								</div>
-								: <>
-									<div className="buttons-wrap">
-										<button onClick = {() => start()}>Start</button>
-										<button onClick = {() => pause()}>Pause</button>
-									</div>
-									<div className="field" 
-										style={{ width: `${width * 40}px`, 
-												height: `${height * 40}px` }}>
-										{[...Array(width * height).keys()].map(item => {
-											return <div className={livingCells.includes(item) ? "alive" : "dead"}
-														data-num={item}
-														key={item}>
-												</div>
-										})}
-									</div>
-								</>
-					}
-				</div>					
-			</div>		
-}
-// class App extends Component {
-// 	constructor(props) {
-// 		super(props)
-// 		this.state = {
-// 			width: "",
-// 			height: "",
-// 			percentLivingCell: "",
-// 			livingCells: [],
-// 			timerId: undefined,
-// 			initial: false
-// 		}
-// 	}
-
-// 	start = () => {
-// 		this.setState({
-// 			timerId: setInterval(() => {
-// 				this.setState({ livingCells: this.liveOrDie() });
-// 				this.exportToConsole();
-// 			}, 1000)
-// 		})
-// 	}
-// 	pause = () => {
-// 		clearTimeout(this.state.timerId);
-// 	}
-// 	liveOrDie() {
-// 		const currentAlive = [];
-
-// 		// for(let i=0; i<this.state.width; i++){
-// 		// 	for(let j=0; j<this.state.height; j++){
-			
-// 		// 	}
-// 		// }
-
-
-// 		[...Array(100).keys()].forEach(el => {
-// 			const neibors = this.getNeighbours(el);
-// 			const aliveNeibors = neibors.filter(item => this.state.livingCells.includes(item));
-
-// 			if ((this.state.livingCells.includes(el) 
-// 			&& (aliveNeibors.length === 2 || aliveNeibors.length === 3)) 
-// 			|| (!this.state.livingCells.includes(el) 
-// 			&& aliveNeibors.length === 3)) {
-// 				currentAlive.push(el)
-// 			} 
-// 		})
-// 		return currentAlive;
-// 	}
-// 	getNeighbours = cell => {		
-// 		const sizeW = +this.state.width;
-// 		const sizeH = +this.state.height;
-// 		const possibleNeibors = [cell + 1, cell - 1, cell - sizeW - 1, cell - sizeW,
-// 								 cell - sizeW + 1, cell + sizeW - 1, 
-// 								 cell + sizeW, cell + sizeW + 1];
-// 		return possibleNeibors.filter(el => el>=0 && el<sizeW*sizeH)
-// 		// try{
-
-// 		// } catch(e){
-
-// 		// } 
-// 		// if (cell % 10 === 9 || cell === 9) {
-// 		// 	return neightbours = [cell - 1, cell - 11, cell - 10, cell + 9, cell + 10]}
-// 		// else if (cell % 10 === 0) {
-// 		// 	return neightbours = [cell - 10, cell - 9, cell + 1, cell + 10, cell + 11]}
-// 		// else neightbours = [cell + 1, cell - 1, cell - 11, cell - 10, cell - 9, cell + 9, cell + 10, cell + 11];
-// 		// return neightbours;
-// 	}
-// 	exportToConsole = () => {
-// 		let str = "";
-// 		for(let i=0; i<this.state.width*this.state.height;i++){
-// 			if (i===0 || i%this.state.width === 0) str+="|";
-// 			this.state.livingCells.includes(i) ? str+="x|" : str+="-|";
-// 			if (i+1===this.state.width || (i+1)%this.state.width === 0) str+="\n";
-// 		}
-// 		console.log(str);
-// 		console.log("=".repeat(this.state.width*2+1));
-// 	}
-// 	init = () => {
-// 		const arr = new Set();
-// 		const amountLivingCell = this.state.width*this.state.height*this.state.percentLivingCell/100
-// 		while (arr.size < amountLivingCell) {
-// 			let number = Math.floor(Math.random() * this.state.width*this.state.height + 1);
-// 			arr.add(number);
-// 		}
-// 		this.setState({ livingCells: Array.from(arr) });
-// 		this.setState({ initial: true });	
-// 	}
-// 	setHeight = (e) => {
-// 		this.setState({ height: e.target.value });	
-// 	}
-// 	setWidth = (e) => {
-// 		this.setState({ width: e.target.value });	
-// 	}
-// 	setAmount = (e) => {
+		console.log("=".repeat(this.state.width*2+1));
+		return str;
 		
-// 		this.setState({ percentLivingCell: parseInt(e.target.value) });	
-// 	}
-// 	render() {
-// 		return  <div style={{ backgroundImage: `url(${backUrl})` }} className="back">
-// 					<div className="field-wrap">
-// 						<h1>Game of Life</h1>
-// 						<h3>test task</h3>
-// 						{!this.state.initial 
-// 						? <div className="init-size-field">
-// 								<span>Введите размер ширины поля</span>
-// 								<input onChange = {(e) => this.setWidth(e)} 
-// 									   value = {this.state.width}/>
-// 								<span>Введите размер высоты поля</span>
-// 								<input onChange = {(e) => this.setHeight(e)} 
-// 								 	   value = {this.state.height}/>
-// 								<span>Введите колличество живых клеток(%)</span>
-// 								<input onChange = {(e) => this.setAmount(e)} 
-// 									   value = {this.state.amountLivingCell}/>
-// 								<button onClick = {() => this.init()}>OK</button>
-// 							</div>
-// 						: <>
-// 							<div className="buttons-wrap">
-// 								<button onClick = {() => this.start()}>Start</button>
-// 								<button onClick = {() => this.pause()}>Pause</button>
-// 							</div>
-// 							<div className="field" 
-// 								 style={{ width: `${this.state.width * 40}px`, 
-// 										height: `${this.state.height * 40}px` }}>
-// 								{[...Array(this.state.width * this.state.height).keys()].map(item => {
-// 									return <div className={this.state.livingCells.includes(item) ? "alive" : "dead"}
-// 												data-num={item}
-// 												key={item}>
-// 										   </div>
-// 								})}
-// 							</div>
-// 						  </>
-// 						}
-// 					</div>					
-// 				</div>		
-// 	}
-// }
+	}
+	compareState = (str) => {
+		if(str === this.state.strState) {
+			this.setState({ gameOver: true })
+			clearTimeout(this.state.timerId);
+		} else {
+			this.setState({ strState: str })
+		}
+	}
+	init = () => {		
+		const cells = this.getLivingCells();
+		this.renderField(cells);
+		this.setState({ initial: true });	
+	}
+	renderField = (cells) => {
+		const field = [];
+		let count = 0;
+
+		for(let i=0; i<this.state.height; i++){
+			field[i]=[];
+			for(let j=0; j<this.state.width; j++){
+				cells.includes(count) ? field[i].push(1) : field[i].push(0);
+				count++;
+			}
+		}
+		this.setState({ field: field});
+	};	
+	getLivingCells = () => {
+		const arr = new Set();
+		const amountLivingCell = this.state.width*this.state.height*this.state.percentLivingCell/100;
+		while (arr.size < amountLivingCell) {
+			let number = Math.floor(Math.random() * this.state.width*this.state.height + 1);
+			arr.add(number);
+		}
+		this.setState({ livingCells: Array.from(arr) });
+		return Array.from(arr);
+	}
+	setHeight = (e) => {
+		this.setState({ height: e.target.value });	
+	}
+	setWidth = (e) => {
+		this.setState({ width: e.target.value });	
+	}
+	setAmount = (e) => {		
+		this.setState({ percentLivingCell: parseInt(e.target.value) });	
+	}
+	render() {
+		return  <div style={{ backgroundImage: `url(${backUrl})` }} className="back">
+					<div className="field-wrap">
+						<h1>Game of Life</h1>
+						<h3>test task</h3>
+						{!this.state.initial 
+						? <div className="init-size-field">
+								<span>Введите размер ширины поля</span>
+								<input onChange = {this.setWidth} 
+									   value = {this.state.width}/>
+								<span>Введите размер высоты поля</span>
+								<input onChange = {this.setHeight} 
+								 	   value = {this.state.height}/>
+								<span>Введите колличество живых клеток(%)</span>
+								<input onChange = {this.setAmount} 
+									   value = {this.state.amountLivingCell}/>
+								<button onClick = {this.init}>OK</button>
+							</div>
+						: <>
+							<div className="buttons-wrap">
+								<button onClick = {this.start}>Start</button>
+								<button onClick = {this.pause}>Pause</button>
+							</div>
+							<div className="field" 
+								 style={{ width: `${this.state.width * 40}px`, 
+										height: `${this.state.height * 40}px` }}>
+								{this.state.field.flat(1).map((item, index) => {
+									return <div className={item===1 ? "alive" : "dead"}
+												data-num={item}
+												key={index}>
+										   </div>
+								})}
+							</div>
+						  </>
+						}
+						{this.state.gameOver && <span className = {"game-over"}>The End!</span>}
+					</div>					
+				</div>		
+	}
+}
 
 export default App;
